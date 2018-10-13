77585558b7a5019c3ade94cf2f65a9ba592ef3e9
Anni Piragauta <aapiragautau@unal.edu.co>
2018-10-12 19:21:41 -0500

    Adding web3 to package.json and injecting web3 into main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Web3 from 'web3'
 
 Vue.config.productionTip = false
 
+// window -> Browser, escucha por el evento load
+// currentProvider -> Metamask (permite comunicarse a la cadena de bloques)
+window.addEventListener('load', () => {
+    if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    }else{
+      console.warn('Not metamask detected')
+    }
+})
+
+//Monta Vue en un elemento con id = app -> App.vue
 new Vue({
   router,
   store,
